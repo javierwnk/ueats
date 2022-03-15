29e2fc7a4a639225fcb168f6c83232fd9731fc77
Javier Wensko <javierwnk2@outlook.com>
2022-03-15 18:54:36 -0300

    Components and View created

---
+++ src/views/HeaderView.vue
@@ -0,0 +1,49 @@
+<template>
+  <header>
+      <div class="logo">
+          <img src="../assets/logo.svg" alt="Uber Eats Logo">
+      </div>
+
+      <div class="searchBar">
+          <input type="search" placeholder="Buscar">
+      </div>
+
+      <nav>
+          <router-link v-for="option in options" :to="option.path" :key="option.index">{{option.name}}</router-link>
+      </nav>
+      
+  </header>
+</template>
+
+<script>
+export default {
+    name: "HeaderView",
+    data() {
+        return {
+            options: [{name: "Login", path: "/login"}, {name: "Sign Up", path: "/singup"}, {name: "Delivery", path: "/delivery"}, {name: "Cart", path: "/cart"}]
+        }
+    }
+}
+</script>
+
+<style scoped>
+    header {
+        height: 96px;
+        width: 100%;
+        border-bottom: 2px solid #E2E2E2;
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        align-content: center;
+    }
+
+    .logo {
+        display: flex;
+        justify-content: center;
+    }
+
+    .searchBar {
+        background-color: #EEEEEE;
+    }
+
+
+</style>
