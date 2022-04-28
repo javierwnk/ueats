df072a40986b922d44d7e8ad85e19f39a8137bff
Javier Wensko <javierwnk2@outlook.com>
2022-04-28 16:33:59 -0300

    Cart design done

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -17,16 +17,29 @@
                   <td>{{product.name}}</td>
                   <td>${{product.price}}</td>
                   <td>{{product.qty}}</td>
-                  <td><button @click="deleteItem(product.id)">Delete</button></td>
+                  <td><button @click="deleteItem(product.id)"><b-icon icon="trash"></b-icon></button></td>
               </tr>
               <tr>
                   <td>Total:</td>
-                  <td>{{orderTotal}}</td>
+                  <td>${{orderTotal}}</td>
               </tr>
           </tbody>
       </table>
+      <form action=""  v-if="cart.length != 0">
 
-      <button @click="makeOrder">Make order</button>
+          <p>Complete your address details and complete your order</p>
+
+          <label for="name">Address</label>
+            <input
+            type="address" 
+            name="address" 
+            id="address"
+            placeholder="Address"
+            >
+
+          <button @click="makeOrder">Make order</button>
+      </form>
+
   </div>
 </template>
 
@@ -52,7 +65,7 @@ export default {
     computed: {
         orderTotal() {
            let total = 0
-           this.cart.forEach(product => total += product.price)
+           this.cart.forEach(product => total += Number(product.price))
             return total
         }
     },
@@ -68,9 +81,68 @@ export default {
 <style scoped>
     .container {
         margin-top: 30px;
+        display: grid;
+        grid-template-areas: "resume form";
+        column-gap: 32px;
+    }
+
+    table {
+        grid-area: resume;
+    }
+
+    table button {
+        width: 80px;
+        border: 0;
+        background: transparent;
+    }
+
+    form {
+        grid-area: form;
+        background: rgb(249, 249, 249);
+        border-radius: 50px;
+        padding: 24px;
+        display: flex;
+        flex-flow: column wrap;
+        row-gap: 12px;
+    }
+
+    form p {
+        text-align: center;
+    }
+
+    form input {
+        background: transparent;
+        border: 0;
+        border-bottom: 1px solid rgb(206, 206, 206);
+    }
+
+    form input:focus, form button:focus {
+        outline: none;
+    }
+
+    form button {
+        background: black;
+        color: white;
+        border: 0;
+        border-radius: 500px;
+
+        height: 40px;
+    }
+
+    label {
+        display: none;
+    }
+
+    th {
+        text-align: center;
     }
     
     td {
         padding: 20px;
+    }
+
+    td img {
+        width: 200px;
+
     }
 </style>
